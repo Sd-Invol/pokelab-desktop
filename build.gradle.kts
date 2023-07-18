58bec3f34b8c672aa8290974e2807eaf545dc0d0
Xuanang Zhao <xuanangz@google.com>
2023-07-18 12:26:44 +0800

    To material3

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ kotlin {
     val jvmMain by getting {
       dependencies {
         implementation(compose.desktop.currentOs)
+        implementation("org.jetbrains.compose.material3:material3-desktop:1.4.1")
       }
     }
     val jvmTest by getting
--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,7 +1,7 @@
-import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
